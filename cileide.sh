9459e4af382744f635875739a07082afeda61296
FSECanada <fullstackelectron@gmail.com>
2022-07-17 17:32:30 -0400

    installing device if not present

--- cileide.sh
+++ cileide.sh
@@ -2,12 +2,12 @@
 # weston &
 # sleep 5s # could be less
 #export WAYLAND_DISPLAY=wayland-0
-#export DISPLAY=:1.0
 #export QT_QPA_PLATFORM=minimal
 #export QT_QPA_PLATFORM_PLUGIN_PATH=/usr/lib/plugins/platforms
 # echo shtc1 0x70 > /sys/bus/i2c/devices/i2c-1/new_device
 export QT_QPA_PLATFORM=wayland
-
+export DISPLAY=0
+export XDG_RUNTIME_DIR=/run/user/0
 
 export QT_PLUGIN_PATH=/usr/lib/plugins
 export QT_QPA_FONTDIR=/usr/share/fonts/truetype
--- src/sensor.py
+++ src/sensor.py
@@ -6,12 +6,29 @@ class Sensors:
         self.config = Config().get('sensor')
         self.temp_humid_sensor = self.config.get('temp_hum_sensor')
         self.sensor_hw = Hwmon.HW()
+        self.check_or_install_sensor()
+        
+        
 
+    def install_sensor(self, sensor):
+        try:
+            sensor_id = sensor.get("sensor_identification")
+            with open(sensor.get('sys_folder'), "w") as file_object:
+                file_object.write(sensor.get('sensor_identification'))
+        except Exception as e:
+            print(f"Could not install sensor with id: '{sensor_id}'")
+        
+    def check_or_install_sensor(self):
+        sensor_data = self.sensor_hw.data()
+        if not sensor_data.get(self.temp_humid_sensor.get('name'), {}):
+            self.install_sensor(self.temp_humid_sensor)
+                    
+        
     @property
     def humidity(self):
         try:
             if data := self.sensor_hw.data():
-                humidity = float(data.get(self.temp_humid_sensor, {}).get('humidity1',0))
+                humidity = float(data.get(self.temp_humid_sensor.get('name'), {}).get('humidity1',0))
                 return int(humidity)                                               
         except Exception as e:                       
             print(f"Error while reading device: {e}")
@@ -21,7 +38,7 @@ class Sensors:
     def temp_c(self):
         try:                                 
             if data := self.sensor_hw.data():                                          
-                temp_c = float(data.get(self.temp_humid_sensor, {}).get('temp1', 0.0)) 
+                temp_c = float(data.get(self.temp_humid_sensor.get('name'), {}).get('temp1', 0.0)) 
                 return round(temp_c, 1)                                                
         except Exception as e:                       
             print(f"Error while reading device: {e}")
